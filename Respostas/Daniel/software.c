34a8fb7643f9b407add504449cbab64c6d7d47b3
danieldcsoares <daniel.dayrellcs@gmail.com>
2025-03-09 22:13:56 -0300

    trabalho em prog

---
+++ Respostas/Daniel/software.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "software.h"
+
+struct Software{
+    char* nome;
+    char* categoria;
+    int impacto;
+    char* motivo;
+    int tempoEstimado;
+};
+
+
+/**
+ * @brief Aloca uma estrutura Software na memória e inicializa os parâmetro necessários
+ * @param nome Nome do software
+ * @param categoria Categoria do software
+ * @param impacto Impacto do chamado
+ * @param motivo Motivo da abertura do ticket
+ * @return  Uma estrutura Software inicializada.
+ */
+Software *criaSoftware(char *nome, char *categoria, int impacto, char *motivo){
+    Software* s = (Software*) malloc (sizeof(Software));
+    /*s->nome = (char*) malloc(sizeof(char) * MAX_TAM_NOME_SOFTWARE);
+    s->categoria = (char*) malloc(sizeof(char) * MAX_TAM_CAT);
+    s->motivo = (char*) malloc(sizeof(char) * MAX_TAM_MOTIVO);*/
+
+    s->impacto = impacto;
+    strcpy(s->nome, nome);
+    strcpy(s->categoria, categoria);
+    strcpy(s->motivo, motivo);
+
+    return s;
+}
+
+/**
+ * @brief Lê da entrada padrão um  ticket do TIPO software
+ * @return  Um chamado do Tipo Software
+ */
+Software *lerSoftware(){
+    Software* s = (Software*) malloc (sizeof(Software));
+    s->nome = (char*) malloc(sizeof(char) * MAX_TAM_NOME_SOFTWARE);
+    s->categoria = (char*) malloc(sizeof(char) * MAX_TAM_CAT);
+    s->motivo = (char*) malloc(sizeof(char) * MAX_TAM_MOTIVO);
+
+    scanf("%[^\n]\n", s->nome);
+    scanf("%[^\n]\n", s->categoria);
+    scanf("%d\n", &s->impacto);
+    scanf("%[^\n]\n", s->motivo);
+
+    return s;
+}
+
+/**
+ * @brief  Calcula o tempo estimado para resolver um ticket do tipo Software.
+ * Tempo estimado depende da categoria e impacto
+ * @param s  Ticket do tipo Software
+ */
+void setTempoEstimadoSoftware(Software *s){
+    if(strcmp(s->categoria, "BUG") == 0){
+        s->tempoEstimado = 3;
+    }
+    else{
+        if(strcmp(s->categoria, "OUTROS") == 0){
+            s->tempoEstimado = 2;
+        }
+        else{
+            if(strcmp(s-> categoria, "DUVIDA") == 0){
+                s->tempoEstimado = 1;
+            }
+        }
+    }
+
+    s->tempoEstimado += s->impacto;
+}
+
+/**
+ * @brief  Retorna o tempo estimado para resolver um ticket do tipo Software.
+ * @param dado  Ticket do tipo Software
+ * @return  Tempo estimado para resolver um ticket do tipo Software
+ */
+int getTempoEstimadoSoftware(void *dado){
+    Software* s = (Software*) dado;
+    return s->tempoEstimado;
+}
+
+/**
+ * @brief  Retorna o tipo do ticket
+ * @return  'S' para Software
+ */
+char getTipoSoftware(){
+    return 'S';
+}
+
+/**
+ * @brief  Desaloca um ticket do tipo Software da memória
+ * @param s  Ticket do tipo Software
+ */
+void desalocaSoftware(void *s){
+    if(s != NULL){
+        Software* sw = (Software*) s;
+        free(sw->nome);
+        free(sw->categoria);
+        free(sw->motivo);
+        free(sw);
+    }
+}
+
+/**
+ * @brief  Imprime um ticket do tipo Software
+ * @param dado  Ticket do tipo Software
+ */
+void notificaSoftware(void *dado){
+    Software* s = (Software*) dado;
+    printf("- Tipo: Software\n");
+    printf("- Nome do software: %s\n", s->nome);
+    printf("- Categoria: %s\n", s->categoria);
+    printf("- Nível do impacto: %d\n", s->impacto);
+    printf("- Motivo: %s\n", s->motivo);
+    printf("- Tempo estimado: %dh\n", s->tempoEstimado);
+}
---
+++ Resultados/Daniel/ticket/ticket.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ticket.h"
+#include <string.h>
+
+struct Ticket{
+    char* cpf;
+    char* id;
+    void* dado;
+    func_ptr_tempoEstimado tempo;
+    func_ptr_tipo tipo;
+    func_ptr_notifica notifica;
+    func_ptr_desaloca desaloca;
+    char status;
+};
+
+
+/**
+ * @brief Aloca uma estrutura Ticket na memória e inicializa os parâmetro necessários
+ * @param cpfSol CPF de quem está solicitando a abertura do ticket
+ * @param dado   Um ticket genérico  (considerando que existe mais de um tipo de ticket)
+ * @param getTempo  Função de callback que retorna o tempo para aquele ticket ser resolvido
+ * @param getTipo   Função de callback que retorna o tipo do ticket ser resolvido
+ * @param notifica  Função de callback de notificação  (impressãode um ticket
+ * @param desaloca  Função de callback que irá desalocar  um ticket da memória
+ * @return  Uma estrutura Ticket inicializada.
+ */
+Ticket *criaTicket(char *cpfSol, void *dado, func_ptr_tempoEstimado getTempo, func_ptr_tipo getTipo, func_ptr_notifica notifica, func_ptr_desaloca desaloca){
+    
+    Ticket* t = (Ticket*) malloc(sizeof(Ticket));
+    t->cpf = (char*) malloc (MAX_TAM_CPF * sizeof(char));
+    t->id = (char*) malloc(MAX_TAM_ID * sizeof(char));
+    //t->dado = (void*) malloc(sizeof(void));
+    
+    strcpy(t->cpf, cpfSol);
+    //t->cpf = strdup(cpfSol);
+    t->tempo = getTempo;
+    t->tipo = getTipo;
+    t->notifica = notifica;
+    t->desaloca = desaloca;
+    t->dado = dado;
+    t->status = 'A';
+
+    return t;
+
+}
+
+/**
+ * @brief Atribui um ID a um ticket
+ * @param d Ticket inicializado
+ * @param id ID a ser atribuido ao ticket
+ */
+void setIDTicket(Ticket *d, char *id){
+    //d->id = strdup(id);
+    strcpy(d->id, id);
+}
+
+/**
+ * @brief Finaliza um ticket
+ * @param t Ticket inicializado
+ */
+void finalizaTicket(Ticket *t){
+    t->status = 'F';
+}
+
+/**
+ * @brief Recupera o CPF de quem solicitou a abertura do ticket
+ * @param t Ticket inicializado
+ * @return CPF de quem solicitou a abertura do ticket
+ */
+char *getCPFSolicitanteTicket(Ticket *t){
+    return t->cpf;
+}
+
+/**
+ * @brief Recupera o tempo estimado para resolver um ticket
+ * @param t Ticket inicializado
+ * @return tempo estimado para resolver um ticket
+ */
+int getTempoEstimadoTicket(Ticket *t){
+    return t->tempo(t->dado);
+}
+
+/**
+ * @brief Recupera o tipo de um ticket
+ * @param t Ticket inicializado
+ * @return tipo do Ticket
+ */
+char getTipoTicket(Ticket *t){
+    return t->tipo(t->dado);
+}
+
+/**
+ * @brief Recupera o status de um ticket
+ * @param t Ticket inicializado
+ * @return status do Ticket
+ */
+char getStatusTicket(Ticket *t){
+    return t->status;
+}
+
+/**
+ * @brief  Desaloca um ticket da memória
+ * @param doc estrutura do tipo Ticket que deve ser liberada da memória
+ */
+void desalocaTicket(Ticket *doc){
+    if(doc != NULL){
+        free(doc->cpf);
+        free(doc->id);
+        free(doc->dado);
+        doc->desaloca(doc);
+    }
+}
+
+/**
+ * @brief  Notifica (imprime) um ticket
+ * @param doc Ticket a ser notificado
+ */
+void notificaTicket(Ticket *doc){
+    printf("---------TICKET-----------\n");
+    printf("- ID: %s\n", doc->id);
+    printf("- Usuario solicitante: %s\n", doc->cpf);
+    
+    doc->notifica(doc->dado);
+
+    printf("- Status: ");
+    if(doc->status == 'A'){
+        printf("Aberto\n");
+    }
+    else{
+        printf("Finalizado\n");
+    }
+    printf("-------------------------\n\n");
+}
